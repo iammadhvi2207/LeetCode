b015a430dd4378a418f272b178b1e23388f8483b
Madhvi Mishra <madhvimishra2207@gmail.com>
2026-04-05 02:37:01 +0530

    Second Question

--- 2.add-two-numbers.java
+++ 2.add-two-numbers.java
@@ -5,6 +5,9 @@
  */
 
 // @lc code=start
+
+import java.util.List;
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -17,7 +20,25 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head =new ListNode(0);
+        ListNode result = head;
+        int carry =0;
         
+        while (l1!=null || l2!=null)
+        {
+            if (l1==null)
+                l1 = new ListNode(0);
+            if (l2==null)            
+                l2 = new ListNode(0);
+            result.next = new ListNode((l1.val+l2.val+carry)%10); 
+            carry = (l1.val+l2.val+carry)/10;
+            l1 = l1.next;
+            l2 = l2.next;
+            result = result.next;
+        }
+        if (carry>0)
+            result.next = new ListNode(carry);
+        return head.next;
     }
 }
 // @lc code=end
