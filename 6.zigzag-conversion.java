1e1e765a75f6adb9e0a799034999021668bcfe68
Madhvi Mishra <madhvimishra2207@gmail.com>
2026-04-09 01:55:02 +0530

    Trying Fifth

--- 6.zigzag-conversion.java
+++ 6.zigzag-conversion.java
@@ -8,6 +8,21 @@
 class Solution {
     public String convert(String s, int numRows) {
         
+        int c=0;
+        String result="";
+        for (int i=1;i<=numRows;i++)
+        {
+            while(c>s.length())
+            {
+
+                result=result+s.charAt(c);
+                c+=numRows;
+            }
+            c=i;
+
+        }
+        return result;
+
     }
 }
 // @lc code=end
