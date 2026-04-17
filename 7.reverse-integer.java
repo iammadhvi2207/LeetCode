46c2a433a4c83e2c05d3651997570ee1dbe0dfc9
Madhvi Mishra <madhvimishra2207@gmail.com>
2026-04-18 01:07:45 +0530

    Seventh Question

--- 7.reverse-integer.java
+++ 7.reverse-integer.java
@@ -7,23 +7,16 @@
 // @lc code=start
 class Solution {
     public int reverse(int x) {
-        int c=0;
-        for (int i=x;i!=0;i/=10)
-        {
-            c++;
-
-        }
         int result =0;
         for (int i=x;i!=0;i/=10)
         {
             int temp=(int)i%10;
-            System.out.println(temp+" ");
-            result = result + (int)(temp * Math.pow(10,--c));
+            if (result > Integer.MAX_VALUE / 10 || result < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+            result = result*10 + temp ;
 
         }
-
-        if(x<0 && result>0)
-            result = -result;
         return result;
     }
 }
