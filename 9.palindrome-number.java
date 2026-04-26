057f5665aff66e45f00f07c7a51b7fb3cd5d3d81
Madhvi Mishra <madhvimishra2207@gmail.com>
2026-04-27 00:01:17 +0530

    Nienth Question

---
+++ 9.palindrome-number.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=9 lang=java
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x<0)
+            return false;
+        int rev=0;
+        for (int i=x; i>0; i=i/10)
+        {
+            rev= rev*10 + i%10;
+        }
+        return rev==x;
+    }
+}
+// @lc code=end
+
