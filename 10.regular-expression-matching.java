18101935351357ec2de86d069dfdf188a104985a
Madhvi Mishra <madhvimishra2207@gmail.com>
2026-04-30 00:20:05 +0530

    Trying Question Tenth

--- 10.regular-expression-matching.java
+++ 10.regular-expression-matching.java
@@ -7,7 +7,25 @@
 // @lc code=start
 class Solution {
     public boolean isMatch(String s, String p) {
-        
+        if (p==null){return false;}
+        else if(s.equals(p)){return true;}
+        else {
+            boolean result = true;
+            int j=0;
+            for (int i=0; i<s.length(); i++)
+            {
+                char ch_p = p.charAt(j);
+                if(ch_p == '.')
+                    continue; j++;
+                if(ch_p == '*')
+                {
+                    char ch = s.charAt(i-1);
+                    ch_p = ch;
+                }
+                
+            }
+            return result;
+        }
     }
 }
 // @lc code=end
