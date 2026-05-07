d2bf84d4bee626717dd30464c2af8b6094fc7215
Madhvi Mishra <madhvimishra2207@gmail.com>
2026-05-07 22:32:47 +0530

    Solved Tenth Problem

--- 10.regular-expression-matching.java
+++ 10.regular-expression-matching.java
@@ -7,26 +7,31 @@
 // @lc code=start
 class Solution {
     public boolean isMatch(String s, String p) {
-        if (p==null){return false;}
-        else if(s.equals(p)){return true;}
-        else {
-            boolean result = true;
-            int j=0;
-            for (int i=0; i<s.length(); i++)
-            {
-                char ch_p = p.charAt(j);
-                if(ch_p == '.')
-                    continue; j++;
-                if(ch_p == '*')
-                {
-                    char ch = s.charAt(i-1);
-                    ch_p = ch;
-                    continue;
-                }
-                
-            }
-            return result;
+            return match(s, p, 0, 0);
         }
+    private boolean match(String s, String p, int i, int j) {
+
+        // Pattern finished
+        if (j == p.length()) {
+            return i == s.length();
+        }
+
+        // Check if current characters match
+        boolean firstMatch = (i < s.length() &&
+                (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.'));
+
+        // If next character in pattern is '*'
+        if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+
+            // Two choices:
+            // 1. Ignore x* completely
+            // 2. Use x* if first character matches
+            return match(s, p, i, j + 2) ||
+                   (firstMatch && match(s, p, i + 1, j));
+        }
+
+        // Normal character match
+        return firstMatch && match(s, p, i + 1, j + 1);
     }
 }
 // @lc code=end
