7b50cf9adefb6bc931dcd2b96a25387e0f85d9bb
Madhvi Mishra <madhvimishra2207@gmail.com>
2026-05-07 22:35:44 +0530

    Solved Tenth Problem

--- 10.regular-expression-matching.java
+++ 10.regular-expression-matching.java
@@ -11,26 +11,19 @@ class Solution {
         }
     private boolean match(String s, String p, int i, int j) {
 
-        // Pattern finished
         if (j == p.length()) {
             return i == s.length();
         }
-
-        // Check if current characters match
         boolean firstMatch = (i < s.length() &&
                 (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.'));
 
-        // If next character in pattern is '*'
         if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
 
-            // Two choices:
-            // 1. Ignore x* completely
-            // 2. Use x* if first character matches
             return match(s, p, i, j + 2) ||
                    (firstMatch && match(s, p, i + 1, j));
         }
 
-        // Normal character match
+        
         return firstMatch && match(s, p, i + 1, j + 1);
     }
 }
