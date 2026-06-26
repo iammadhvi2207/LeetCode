a01a89a5c6472be5fe228ee8cb0d94d745a2e155
Madhvi Mishra <madhvimishra2207@gmail.com>
2026-06-27 01:05:38 +0530

    Completed 11th

--- 11.container-with-most-water.java
+++ 11.container-with-most-water.java
@@ -7,24 +7,19 @@
 // @lc code=start
 class Solution {
     public int maxArea(int[] height) {
-        int[] idx= new int[height.length];
-        for (int i=0;i< height.length;i++)
-            idx[i] = i;
-
-        sort(height, idx, 0, idx.length-1);
 
         int area = 0;
-        for (int i = 0;i<idx.length;i++)
+        for (int i = 0;i<height.length;i++)
         {
-            if(height[i] * (idx.length - 1) <= area)
-                break;
-            for (int j=i+1;j<idx.length;j++)
+            if(height[i] * (height.length - 1) <= area)
+                continue;
+            for (int j=i+1;j<height.length;j++)
             {
-                if(height[j] * (idx.length - 1) <= area)
-                    break;
-                int len = height[j];
-                int bre = Math.abs(idx[i]-idx[j]);
-                int a = len * bre;
+                if(height[i] * (height.length - 1) <= area)
+                    continue;
+                int min = Math.min(height[i], height[j]);
+                int bre = Math.abs(i-j);
+                int a = min * bre;
 
                 if(a>area)
                     area =a;
@@ -35,46 +30,6 @@ class Solution {
 
     }
 
-    public int partition(int[] a, int [] b, int l , int h)
-    {
-        int pivot = a[h];
-        int i= l-1;
-        for (int j=l;j<h;j++)
-        {
-            if(a[j]>pivot)
-            {
-                i++;
-                int temp = a[i];
-                a[i]=a[j];
-                a[j]=temp;
-
-                temp = b[i];
-                b[i]=b[j];
-                b[j]=temp;
-            }
-        }
-
-        int temp = a[i+1];
-        a[i+1]=a[h];
-        a[h]= temp;
-
-        temp = b[i+1];
-        b[i+1]=b[h];
-        b[h]= temp;
-
-        return i+1;
-    }
-
-    void sort(int[] a, int [] b, int l , int h)
-    {
-        if(l<h)
-        {
-            int pi = partition(a,b,l,h);
-
-            sort(a,b,l,pi-1);
-            sort(a,b,pi+1,h);
-        }
-    }
 }
 // @lc code=end
 
