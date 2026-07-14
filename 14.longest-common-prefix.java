d6116242f87fadbcde5db34b8aa5309e5d0e81b4
Madhvi Mishra <madhvimishra2207@gmail.com>
2026-07-14 23:55:42 +0530

    Solving 14th

---
+++ 14.longest-common-prefix.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=14 lang=java
+ *
+ * [14] Longest Common Prefix
+ */
+
+// @lc code=start
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        // int len=0, idx=0;
+        // for (int i =0; i< strs.length;i++)
+        // {
+        //     if(len>strs[i].length())
+        //     {
+        //         len=strs[i].length();
+        //         idx=i;
+        //     }
+        // }
+        String str=strs[0].substring(0,str[0].length());
+
+        for(int i=1;i< strs[idx].length();i++)
+        {
+            
+
+        }
+    }
+}
+// @lc code=end
+
