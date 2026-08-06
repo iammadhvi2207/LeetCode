07246dc09dec321f9b88105edde3018f562e4ed3
Madhvi Mishra <madhvimishra2207@gmail.com>
2026-08-07 01:56:57 +0530

    Solved Letter Combination

--- 17.letter-combinations-of-a-phone-number.java
+++ 17.letter-combinations-of-a-phone-number.java
@@ -7,26 +7,51 @@
 // @lc code=start
 class Solution {
     public List<String> letterCombinations(String digits) {
-        Hashmap<Integer,String> map = new Hashamp<>(Map.of(
-            2 , "abc",
-            3 , "def",
-            4 , "ghi",
-            5 , "jkl",
-            6 , "mno",
-            7 , "pqrs",
-            8 , "tuv",
-            9 , "wxyz"
+        List<String> result = new ArrayList<>();
+        if(digits == null || digits.length() == 0 )   return result;
+        
+        HashMap<Character,String> map = new HashMap<>(Map.of(
+            '2' , "abc",
+            '3' , "def",
+            '4' , "ghi",
+            '5' , "jkl",
+            '6' , "mno",
+            '7' , "pqrs",
+            '8' , "tuv",
+            '9' , "wxyz"
         ));
 
-        List<String> s = [];
-        for(int i=0;i<digits.length();i++)
-        {
-            int n = Integer.parseInt(digits.charAt(i));
-            String str = map.get(n);
-            s[i]= str;
+        
+        backtrack(digits, 0, new StringBuilder(), result, map);
 
+        return result;
+    }
+
+    private void backtrack(String digits, int index,StringBuilder current, List<String> result, HashMap<Character, String> map) 
+    {
+
+        if (index == digits.length()) 
+        {
+            result.add(current.toString());
+            return;
+        }
+
+        String letters = map.get(digits.charAt(index));
+
+        for (char ch : letters.toCharArray()) 
+        {
+
+            current.append(ch);
+
+            backtrack(digits, index + 1, current, result, map);
+
+            current.deleteCharAt(current.length() - 1);
         }
     }
+
+        
+        
+    
 }
 // @lc code=end
 
