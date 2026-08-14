38b0504ae7602d47e1d93250209dc4ce7f6dc583
Madhvi Mishra <madhvimishra2207@gmail.com>
2026-08-14 23:12:39 +0530

    Cycle Linked List

--- 141.linked-list-cycle.java
+++ 141.linked-list-cycle.java
@@ -23,8 +23,8 @@ public class Solution {
             return false;
 
         ListNode slow = head;
-        ListNode fast = head.next;
-        while(slow.next != null && fast.next.next != null)
+        ListNode fast = head.next.next;
+        while(fast != null && fast.next != null)
         {
             slow = slow.next;
             fast= fast.next.next;
